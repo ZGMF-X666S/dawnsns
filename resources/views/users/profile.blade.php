b5d5424cc682817e36cf395f8798a5b7307df613
西田 <autumn.shower.rw6@gmail.com>
2024-03-01 18:11:28 +0900

    プロフィールの表示まで

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.login')
 
 @section('content')
+{!! Form::open(['url' => 'profile']) !!}
+    <div class="form-group">
+        {{ Form::input('text', 'username', null, ['required', 'class' => 'form-control', 'placeholder' => 'Username'])}}
+    </div>
+    <div class="form-group">
+        {{ Form::email('mail', null, ['class' => 'form-control','id' => 'inputEmail','placeholder' => 'Eメール'])}}
+    </div>
+    <div class="form-group">
+        現在のパスワード
+        {{ Form::input('text','','●●●●●●●', ['class' => 'form-control','id' => 'inputPassword','placeholder' => 'パスワード'])}}
+    </div>
+    <div class="form-group">
+        新しいパスワード
+    {{ Form::input('text','password','', ['class' => 'form-control','id' => 'inputPassword','placeholder' => 'パスワード'])}}
+    </div>
+    
+    <button type="submit" class="btn btn-success pull-right">更新</button>
+{!! Form::close() !!} 
+
+
 
 
 
